d128bbf28da2d77c6606dcb3368876dd0f42ea9b
minsu <splendent77@gmail.com>
2023-01-12 17:10:59 +0900

    :bulb: [ Update ] log 모델 추가

--- log/models.py
+++ log/models.py
@@ -2,3 +2,13 @@ from django.db import models
 from user.models import User
 
 
+class Log(models.Model):
+    class Meta:
+        db_table = 'Log'
+        ordering = ['-created_at']
+
+    author=models.ForeignKey(User, on_delete = models.CASCADE, related_name='author')
+    money=models.IntegerField()
+    content = models.CharField(max_length=50)
+    category=models.CharField(max_length=10) # 선택지
+    created_at = models.DateTimeField(auto_now_add=True)
